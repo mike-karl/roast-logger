cfd90a1d5d801db00f38d1aaa63eb88b27add6bc
mike <47261547+mike-karl@users.noreply.github.com>
2024-08-22 04:33:30 +0000

    Updated the code to programatically add url paths for api requests and cors policy so that when forked theres no need to manually configure it. Should also work when ran on local machine

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,11 +1,17 @@
+const hostname = require("os").hostname;
+const port = process.env.PORT;
+
+const previewUrl = `https://${hostname}-${port}.csb.app`;
+
+console.log(previewUrl);
+
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'https://vc6ymm-3000.csb.app',
-    'https://vc6ymm-5000.csb.app',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:5000',
+  "https://www.yoursite.com",
+  previewUrl,
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:5000",
 ];
 
 module.exports = allowedOrigins;
--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,21 +1,47 @@
-import axios from "axios"
+import axios from "axios";
 
-const port = "3000";
-export const BASE_URL = process.env.REACT_APP_CODESANDBOX === 'true'
-    ? `https://vc6ymm-${port}.csb.app/`
+const REGEX = /(?<id>\w{5,6})-(?<port>\d{1,5})\.(?<hostname>.*)/;
+
+function getPreviewUrl(port) {
+  const currentUrl = window.location.host;
+
+  console.log(window.location.host);
+
+  const currentMatch = currentUrl.match(REGEX);
+
+  if (!currentMatch?.groups) {
+    return undefined;
+  }
+  const { id, hostname } = currentMatch.groups;
+
+  console.log(id);
+  console.log(hostname);
+  console.log(port);
+
+  if (!id || !port || !hostname) {
+    return undefined;
+  }
+
+  return `${id}-${port}.${hostname}`;
+}
+
+const port = process.env.REACT_APP_SERVER_PORT ?? "3000";
+export const BASE_URL =
+  process.env.REACT_APP_CODESANDBOX === "true"
+    ? `https://${getPreviewUrl(port)}`
     : `http://localhost:${port}/`;
 
 export default axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-type": "application/json",
-    }
+  baseURL: BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-type": "application/json",
-},
-withCredentials: true
+  baseURL: BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+  withCredentials: true,
 });
